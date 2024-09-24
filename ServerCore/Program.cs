b025c769b0b3a8658acef267dc8693936add32c8
심재천 <shimnaldo98@gmail.com>
2024-09-24 16:36:40 +0900

    feat : 메모리 베리어 (메모리 재배치 억제)

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -10,29 +10,56 @@ namespace ServerCore
 {
     class Program
     {
-        static void MainThread(object state)
+        static int x = 0;
+        static int y = 0;
+        static int r1 = 0;
+        static int r2 = 0;
+
+        static void Thread_1()
         {
-            for (int i = 0; i < 5; i++)
-                Console.WriteLine("Hello Thread");
+            y = 1; // Store y
+
+            // 메모리 베리어
+            //  1. 코드 재배치 억제 (현 문제상황)
+            //      1) Full Memory Barrier (ASM MFENCE, C# Thread.MemoryBarrier) : Store/Load 둘다 막는다.
+            //      2) Store Memory Barrier (ASM SFENCE) : Store만 막는다.
+            //      2) Load Memory Barrier (ASM LFENCE) : Load만 막는다.
+
+            // ------------------------------------------
+            Thread.MemoryBarrier();
+
+            r1 = x; // Load x
+        }
+        static void Thread_2()
+        {
+            x = 1; // Store x
+
+            // ------------------------------------------
+            Thread.MemoryBarrier();
+
+            r2 = y; // Load y
         }
 
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(5, 5);
-
-            for (int i = 0; i < 5; i++)
+            int count = 0;
+            while(true)
             {
-                // LongRunning을 사용해 스레드 풀의 스레드를 사용하는 것이 아닌
-                // 별도의 스레드로 동작시킴.
-                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
-                t.Start();
+                count++;
+                x = y = r1 = r2 = 0;
+
+                Task t1 = new Task(Thread_1);
+                Task t2 = new Task(Thread_2);
+                t1.Start();
+                t2.Start();
+                
+                Task.WaitAll(t1, t2);
+
+                if (r1 == 0 && r2 == 0)
+                    break;
             }
 
-            // 코드 실행됨.
-            ThreadPool.QueueUserWorkItem(MainThread);
-
-            while (true){ }
+            Console.WriteLine($"{count}번 만에 빠져나옴.");
         }
     }
 }
