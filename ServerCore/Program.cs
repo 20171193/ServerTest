2d38cd0dd46e0773618ecb055f636935c86dcee8
심재천 <shimnaldo98@gmail.com>
2024-12-31 19:18:41 +0900

    feat : 이벤트 락 (AutoResetEvent)

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -8,39 +8,26 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class SpinLock
+    class Lock
     {
-        volatile int _locked = 0;
+        // bool <- 커널
+        AutoResetEvent _available = new AutoResetEvent(true);
 
         public void Acquire()
         {
-            // 스핀락을 통한 반복적 확인
-            while(true)
-            {
-                int expected = 0;
-                int desired = 1;
-
-                // _locked 값이 expected 값이라면
-                // desired에 할당
-                if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
-                    break;  // 락 획득 시 break
-
-                // 실패 시 ? 
-                Thread.Sleep(1);    // 무조건 휴식 ==> 무조건 1ms 쉼
-                Thread.Sleep(0);    // 조건부 양보 ==> 우선순위가 같거나 높은 스레드를 확인
-                Thread.Yield();     // 관대한 양보 ==> 실행이 가능한 스레드가 있다면 무조건 양보
-            }
+            _available.WaitOne();   // 입장 시도 
+            // _available.Reset(); -> false로 차단 (내포되어 있다고 보면됨.)
         }
         public void Release()
         {
-            _locked = 0;
+            _available.Set();   // 입장을 다시 허용
         }
     }
 
     class Program
     {
         static int _num = 0;
-        static SpinLock _lock = new SpinLock();
+        static Lock _lock = new Lock();
 
         static void Thread_1()
         {
