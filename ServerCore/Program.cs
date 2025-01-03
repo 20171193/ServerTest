034eb0f60a2efbe41ccc53ec029bc46471c467a2
심재천 <shimnaldo98@gmail.com>
2025-01-03 18:35:36 +0900

    feat : TLS (Thread Local Storage) 실습

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -10,35 +10,40 @@ namespace ServerCore
 {
     class Program
     {
-        static volatile int count = 0;
-        static Lock _lock = new Lock();
+        // 스레드 지역
+        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My name is {Thread.CurrentThread.ManagedThreadId}";});
+
+        // 전역
+        static string ThreadName2;
+
+        static void WhoAmI()
+        {
+            /*********** 전역 ************
+             *****************************/
+            //ThreadName2 = $"My name is {Thread.CurrentThread.ManagedThreadId}";
+            //Thread.Sleep(1000);
+            //Console.WriteLine(ThreadName2);
+
+
+            /********* 스레드 지역 *******
+             * 각 스레드가 가지는 TLS 영역
+             *****************************/
+            bool repeat = ThreadName.IsValueCreated;
+            if(repeat)
+                Console.WriteLine($"{ThreadName.Value} is repeat.");
+            else
+                Console.WriteLine(ThreadName.Value);
+        }
 
         static void Main(string[] args)
         {
-            Task t1 = new Task(delegate ()
-            {
-                for(int i =0; i<100000; i++)
-                {
-                    _lock.WriteLock();
-                    count++;
-                    _lock.WriteUnLock();
-                }
-            });
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
 
-            Task t2 = new Task(delegate ()
-            {
-                for(int i =0; i<100000; i++)
-                {
-                    _lock.WriteLock();
-                    count--;
-                    _lock.WriteUnLock();
-                }
-            });
+            // 각 태스크를 부여하여 실행
+            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
 
-            t1.Start();
-            t2.Start();
-            Task.WaitAll(t1, t2);
-            Console.WriteLine(count);
+            ThreadName.Dispose();
         }
     }
 }
